6160b47f18e0aecc3ff8f897166e0d2e317d9795
ignaciodemarchi <ignacio22demarchi@gmail.com>
2023-09-26 20:18:45 -0300

    FoundationStack

--- Solitario/src/main/java/FoundationStack.java
+++ Solitario/src/main/java/FoundationStack.java
@@ -1,58 +1,53 @@
 import java.util.LinkedList;
 import java.util.List;
 
-public class FoundationStack implements Stack<Card>{
+public class FoundationStack implements Stack<Card> {
 
     private int size;
 
-    private List<Card> cards;
+    private final List<Card> cards;
 
     private Suit suit;
 
-    public FoundationStack(){
+    public FoundationStack() {
         size = 0;
         cards = new LinkedList<Card>();
         suit = null;
     }
+    @Override
     public boolean push(Card card) {
 
         if (size > 0 &&
                 (!suit.equals(card.suit()) ||
-                cards.get(size - 1).rank() != card.rank() - 1)){
+                        this.peek().rank() != card.rank() - 1)) {
             return false;
         } else {
             if (size == 0) suit = card.suit();
             cards.add(card);
-            size ++;
+            size++;
             return true;
         }
-    }
-
-    private void privatePush(Card card) {
-        size ++;
-        cards.add(card);
     }
     @Override
     public Card pop() {
         if (this.isEmpty()) return null;
-        size --;
+        size--;
         if (size == 0) suit = null;
         return cards.remove(size - 1);
     }
-
     @Override
     public Card peek() {
         if (this.isEmpty()) return null;
         return cards.get(size - 1);
     }
-
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
-
     @Override
     public int size() {
         return size;
     }
+
+    public boolean verify() { return this.peek().rank() == 13; }
 }
